cb0706e5c341fb197674926485e39e8936e1cdbc
Didrik Sæther <didrik.sather@gmail.com>
2018-03-20 11:57:10 +0100

    Started making the gcd-service

---
+++ gcd/main.go
@@ -0,0 +1,11 @@
+package main
+import (
+"log"
+"net"
+// Change this for your own project
+"github.com/Diddern/gRPC-helloworld/pb"
+context "golang.org/x/net/context"
+"google.golang.org/grpc"
+"google.golang.org/grpc/reflection"
+)
+
